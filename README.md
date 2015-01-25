299d01f5ae23a6501dab40a818cc794373717ef2
Tommy Chen <tommy351@gmail.com>
2015-01-25 17:16:28 +0800

    Rewrite

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # hexo-deployer-heroku
 
-[![Build Status](https://travis-ci.org/hexojs/hexo-deployer-heroku.svg?branch=master)](https://travis-ci.org/hexojs/hexo-deployer-heroku)  [![NPM version](https://badge.fury.io/js/hexo-deployer-heroku.svg)](http://badge.fury.io/js/hexo-deployer-heroku) [![Coverage Status](https://img.shields.io/coveralls/hexojs/hexo-deployer-heroku.svg)](https://coveralls.io/r/hexojs/hexo-deployer-heroku?branch=master) [![Build status](https://ci.appveyor.com/api/projects/status/liqy4nib33ht70so/branch/master?svg=true)](https://ci.appveyor.com/project/tommy351/hexo-deployer-heroku/branch/master)
+[![Build Status](https://travis-ci.org/hexojs/hexo-deployer-heroku.svg?branch=master)](https://travis-ci.org/hexojs/hexo-deployer-heroku)  [![NPM version](https://badge.fury.io/js/hexo-deployer-heroku.svg)](http://badge.fury.io/js/hexo-deployer-heroku) [![Coverage Status](https://coveralls.io/repos/hexojs/hexo-deployer-heroku/badge.svg)](https://coveralls.io/r/hexojs/hexo-deployer-heroku) [![Build status](https://ci.appveyor.com/api/projects/status/liqy4nib33ht70so/branch/master?svg=true)](https://ci.appveyor.com/project/tommy351/hexo-deployer-heroku/branch/master)
 
 Heroku deployer plugin for [Hexo].
 
--- test/index.js
+++ test/index.js
@@ -1,3 +1,99 @@
+var should = require('chai').should();
+var pathFn = require('path');
+var util = require('hexo-util');
+var fs = require('hexo-fs');
+var Promise = require('bluebird');
+var spawn = util.spawn;
+
+var assetDir = pathFn.join(__dirname, '../assets');
+
 describe('Heroku deployer', function(){
-  //
+  var baseDir = pathFn.join(__dirname, 'deployer_test');
+  var publicDir = pathFn.join(baseDir, 'public');
+  var fakeRemote = pathFn.join(baseDir, 'remote');
+  var validateDir = pathFn.join(baseDir, 'validate');
+
+  var ctx = {
+    base_dir: baseDir,
+    public_dir: publicDir,
+    log: {
+      info: function(){}
+    }
+  };
+
+  var deployer = require('../lib/deployer').bind(ctx);
+
+  var port = process.env.PORT = process.env.PORT || 5000;
+
+  function compareFile(a, b){
+    return Promise.all([
+      fs.readFile(a),
+      fs.readFile(b)
+    ]).then(function(result){
+      result[0].should.eql(result[1]);
+    });
+  }
+
+  before(function(){
+    return fs.writeFile(pathFn.join(publicDir, 'foo.txt'), 'foo');
+  });
+
+  beforeEach(function(){
+    // Create a bare repo as a fake remote repo
+    return fs.mkdirs(fakeRemote).then(function(){
+      return spawn('git', ['init', '--bare', fakeRemote]);
+    });
+  });
+
+  after(function(){
+    return fs.rmdir(baseDir);
+  });
+
+  afterEach(function(){
+    return fs.rmdir(fakeRemote).then(function(){
+      return fs.rmdir(validateDir);
+    });
+  });
+
+  function clone(){
+    // Clone the remote repo
+    return spawn('git', ['clone', fakeRemote, validateDir, '--branch', 'master'])
+  }
+
+  function validate(){
+    return clone().then(function(){
+      // Check files
+      return fs.readFile(pathFn.join(validateDir, 'public', 'foo.txt'));
+    }).then(function(result){
+      result.should.eql('foo');
+
+      // Check package.json
+      return compareFile(pathFn.join(assetDir, 'package.json'), pathFn.join(validateDir, 'package.json'));
+    });
+  }
+
+  it('default', function(){
+    return deployer({
+      repo: fakeRemote,
+      silent: true
+    }).then(function(){
+      return validate();
+    });
+  });
+
+  it('server test');
+
+  it.skip('custom message', function(){
+    return deployer({
+      repo: fakeRemote,
+      message: 'custom message',
+      silent: true
+    }).then(function(){
+      return validate();
+    }).then(function(){
+      return spawn('git', ['log', '-1', '--pretty=format:%s'], {cwd: validateDir});
+    }).then(function(content){
+      content.should.eql('custom message');
+    });
+  });
 });
